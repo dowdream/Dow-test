4bfd7113a9680c22d8cec457666f999c690ddcfa
Dow <dowdream@naver.com>
2013-08-01 07:32:31 +0900

    change test.php

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 // 1. 데이터베이스 서버에 접속
-$link=mysql_connect('localhost','root','1818');
+$link=mysql_connect('localhost','root','dkes0903');
 if(!$link) {
 die('Could not connect: '.mysql_error());
 }
